8dacf466cdcd09dea54d3ee75b106336801956d1
Alexandr Chernetskiy <chornoi487@gmail.com>
2019-05-06 13:50:12 +0300

    getAllProducts

--- src/components/product/index.vue
+++ src/components/product/index.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="card">
-<!--    <div class="d-flex justify-content-center">-->
-<!--      <div class="spinner-border  m-5" role="status">-->
-<!--        <span class="sr-only">Loading...</span>-->
-<!--      </div>-->
-<!--    </div>-->
     <img :src="coverImage" class="card-img-top" :alt="title">
     <div class="card-body">
       <h5 class="card-title"> {{ title }}</h5>
--- src/pages/Catalog.vue
+++ src/pages/Catalog.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
 
-    <div class="row">
+    <div class="d-flex justify-content-center" v-if="loading">
+      <div class="spinner-border  m-5" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+
+    <div class="row" v-else>
 
       <div class="col-md-4 my-4" v-for="product in products" :key="product.id">
         <product v-bind="product"/>
@@ -14,12 +20,21 @@
 
 <script>
 import Product from '@/components/product/index'
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
+import { GET_ALL_PRODUCTS } from '@/store/types/actions'
 export default {
   name: 'catalog',
   components: { Product },
   computed: {
-    ...mapState('catalog', ['products'])
+    ...mapState('catalog', ['products', 'loading'])
+  },
+  methods: {
+    ...mapActions('catalog', {
+      getAllProducts: GET_ALL_PRODUCTS
+    })
+  },
+  created () {
+    this.getAllProducts()
   }
 }
 </script>
